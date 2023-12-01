13464080dba8a20f9714f39191bc240ede19c3b2
Lilo002 <lisabasarab@gmail.com>
2023-12-01 21:52:49 +0300

    feat: create enqueue method (Fri, Dec  1, 2023  9:52:49 PM)

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  constructor() {
+    this.queue = null;
+  }
 
   getUnderlyingList() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if (!this.queue) {
+      this.queue = new ListNode(value);
+      return;
+    }
+    let item = this.queue.next;
+    function setValue(value) {
+       if (!item) {
+        item.next = new ListNode(value);
+       } else {
+        item = item.next;
+        setValue(value);
+        return;
+       }
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 }
 
